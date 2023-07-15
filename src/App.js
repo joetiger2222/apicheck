756fe5c82faf2c5e0b3658476cf7f33e8aabf449
yousef mohamed <joetiger22222@gmail.com>
2023-07-15 15:23:43 +0300

    hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  function getCities(){
+    fetch(`http://3algny.somee.com/api/Clinic/Cities`)
+    .then(res=>res.ok?res.json():alert('error'))
+    .then(data=>console.log(data))
+  }
+  useEffect(()=>{
+    getCities();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
